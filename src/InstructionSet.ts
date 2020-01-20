13edadd77e328c32fb932091fc5e59cdd0f589bb
Mat Sz <contact@matsz.dev>
2020-01-20 21:09:21 +0100

    added ASL

--- src/InstructionSet.ts
+++ src/InstructionSet.ts
@@ -62,18 +62,57 @@ const getOperand = (state: State, mode: AddressMode) => {
         case AddressMode.ZEROPAGE_Y:
             return state.memory[(getImmediateByte(state) + state.Y) & 0xff];
         default:
-            return 0;
+            throw new Error('Incorrect address mode');
     }
 };
 
-const createInstruction = (fn: (state: State, operand: number) => State, addressMode: AddressMode, bytes: number) => {
+const setOperand = (state: State, mode: AddressMode, value: number) => {
+    switch (mode) {
+        case AddressMode.ACCUMULATOR:
+            state.A = value;
+            break;
+        case AddressMode.ABSOLUTE:
+            state.memory[getImmediateWord(state)] = value;
+            break;
+        case AddressMode.ABSOLUTE_X:
+            state.memory[getImmediateWord(state) + state.X] = value;
+            break;
+        case AddressMode.ABSOLUTE_Y:
+            state.memory[getImmediateWord(state) + state.Y] = value;
+            break;
+        case AddressMode.INDIRECT_X:
+            state.memory[getWord(state, getImmediateWord(state) + state.X)] = value;
+            break;
+        case AddressMode.INDIRECT_Y:
+            state.memory[getWord(state, getImmediateWord(state)) + state.Y] = value;
+            break;
+        case AddressMode.ZEROPAGE:
+            state.memory[getImmediateByte(state)] = value;
+            break;
+        case AddressMode.ZEROPAGE_X:
+            state.memory[(getImmediateByte(state) + state.X) & 0xff] = value;
+            break;
+        case AddressMode.ZEROPAGE_Y:
+            state.memory[(getImmediateByte(state) + state.Y) & 0xff] = value;
+            break;
+        default:
+            throw new Error('Incorrect address mode');
+    }
+
+    return state;
+};
+
+const createInstruction = (
+    fn: (state: State, operand: number, setOperand?: (value: number) => State) => State,
+    addressMode: AddressMode, bytes: number) => {
+
     return {
         addressMode: addressMode,
         bytes: bytes,
         fn: (state: State) => {
             const operand = getOperand(state, addressMode);
     
-            const result = fn(state, operand);
+            const result = fn(state, operand, (value: number) => setOperand(state, addressMode, value));
             result.PC += bytes;
             
             return result;
@@ -86,23 +125,30 @@ let instructionSet: Instruction[] = [];
 // Based on: https://www.masswerk.at/6502/6502_instruction_set.html
 
 // ADC - Add with carry
-instructionSet[0x69] = createInstruction(ADC, AddressMode.IMMEDIATE,  2);
-instructionSet[0x65] = createInstruction(ADC, AddressMode.ZEROPAGE,   2);
-instructionSet[0x75] = createInstruction(ADC, AddressMode.ZEROPAGE_X, 2);
-instructionSet[0x6D] = createInstruction(ADC, AddressMode.ABSOLUTE,   3);
-instructionSet[0x7D] = createInstruction(ADC, AddressMode.ABSOLUTE_X, 3);
-instructionSet[0x79] = createInstruction(ADC, AddressMode.ABSOLUTE_Y, 3);
-instructionSet[0x61] = createInstruction(ADC, AddressMode.INDIRECT_X, 2);
-instructionSet[0x71] = createInstruction(ADC, AddressMode.INDIRECT_Y, 2);
+instructionSet[0x69] = createInstruction(ADC, AddressMode.IMMEDIATE,   2);
+instructionSet[0x65] = createInstruction(ADC, AddressMode.ZEROPAGE,    2);
+instructionSet[0x75] = createInstruction(ADC, AddressMode.ZEROPAGE_X,  2);
+instructionSet[0x6D] = createInstruction(ADC, AddressMode.ABSOLUTE,    3);
+instructionSet[0x7D] = createInstruction(ADC, AddressMode.ABSOLUTE_X,  3);
+instructionSet[0x79] = createInstruction(ADC, AddressMode.ABSOLUTE_Y,  3);
+instructionSet[0x61] = createInstruction(ADC, AddressMode.INDIRECT_X,  2);
+instructionSet[0x71] = createInstruction(ADC, AddressMode.INDIRECT_Y,  2);
 
 // AND - And with A
-instructionSet[0x29] = createInstruction(AND, AddressMode.IMMEDIATE,  2);
-instructionSet[0x25] = createInstruction(AND, AddressMode.ZEROPAGE,   2);
-instructionSet[0x35] = createInstruction(AND, AddressMode.ZEROPAGE_X, 2);
-instructionSet[0x2D] = createInstruction(AND, AddressMode.ABSOLUTE,   3);
-instructionSet[0x3D] = createInstruction(AND, AddressMode.ABSOLUTE_X, 3);
-instructionSet[0x39] = createInstruction(AND, AddressMode.ABSOLUTE_Y, 3);
-instructionSet[0x21] = createInstruction(AND, AddressMode.INDIRECT_X, 2);
-instructionSet[0x31] = createInstruction(AND, AddressMode.INDIRECT_Y, 2);
+instructionSet[0x29] = createInstruction(AND, AddressMode.IMMEDIATE,   2);
+instructionSet[0x25] = createInstruction(AND, AddressMode.ZEROPAGE,    2);
+instructionSet[0x35] = createInstruction(AND, AddressMode.ZEROPAGE_X,  2);
+instructionSet[0x2D] = createInstruction(AND, AddressMode.ABSOLUTE,    3);
+instructionSet[0x3D] = createInstruction(AND, AddressMode.ABSOLUTE_X,  3);
+instructionSet[0x39] = createInstruction(AND, AddressMode.ABSOLUTE_Y,  3);
+instructionSet[0x21] = createInstruction(AND, AddressMode.INDIRECT_X,  2);
+instructionSet[0x31] = createInstruction(AND, AddressMode.INDIRECT_Y,  2);
+
+// ASL - Arithmetic shift left
+instructionSet[0x0A] = createInstruction(AND, AddressMode.ACCUMULATOR, 1);
+instructionSet[0x06] = createInstruction(AND, AddressMode.ZEROPAGE,    2);
+instructionSet[0x16] = createInstruction(AND, AddressMode.ZEROPAGE_X,  2);
+instructionSet[0x0E] = createInstruction(AND, AddressMode.ABSOLUTE,    3);
+instructionSet[0x1E] = createInstruction(AND, AddressMode.ABSOLUTE_X,  3);
 
 export default instructionSet;
