219c6091698519ba2f3fc43dd1d71451d4f06daf
Mat Sz <contact@matsz.dev>
2020-01-21 13:35:58 +0100

    fixed CF in regular SBC

--- src/instructions/SBC.ts
+++ src/instructions/SBC.ts
@@ -17,7 +17,7 @@ export default function SBC (state: State, operand: number) {
         value = (state.A + ~operand);
         if (state.CF) value++;
     
-        state.CF = value > 0xFF;
+        state.CF = state.A + (state.CF ? 1 : 0) > operand;
         state.ZF = (value & 0xFF) === 0x00;
         state.NF = (value & 0x80) === 0x80;
         state.VF = (~(state.A ^ ~operand) & (state.A ^ value) & 0x80) !== 0x00;
